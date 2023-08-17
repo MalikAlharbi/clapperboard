a13f06b58353a989440764ed5119e5a5214cead7
Malik Alharbi <malik.f.alharbi@gmail.com>
2023-08-18 00:30:36 +0300

    adding functionality + bug fixes

--- frontend/src/components/ShowsCard.js
+++ frontend/src/components/ShowsCard.js
@@ -1,76 +1,84 @@
 import React, { useEffect, useRef, useState } from "react";
 import { fetchInfo, fetchSeasons } from "../ShowsFetch";
 import AddToMyList from "./AddToMyList";
+import Auth from "./Auth";
+import Loading from './Loading'
 import InfoPopup from "./InfoPopUp";
 import { getSavedEpisodes } from "../ApiRequest";
 
-export default function ShowsCard({ showId, name, img, year }) {
+export default function ShowsCard({ showId, name, img, year, isLoggedIn }) {
   const [isPopOpen, setIsPopOpen] = useState(false);
   const [isAddOpen, setIsAddOpen] = useState(false);
   const [isWatching, setIsWatching] = useState(false);
   const [nextEpisode, setNextEpisode] = useState(null);
   const [info, setInfo] = useState(null);
-  const [loading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const infoPopUpRef = useRef(null);
   const addToListPopUpRef = useRef(null);
 
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (
-        infoPopUpRef.current &&
-        !infoPopUpRef.current.contains(event.target)
-      ) {
-        setIsPopOpen(false);
-      }
-      if (
-        addToListPopUpRef.current &&
-        !addToListPopUpRef.current.contains(event.target)
-      ) {
-        setIsAddOpen(false);
-      }
-    }
-    document.addEventListener("mousedown", handleClickOutside);
+  function handleClickOutside(event) {
+    if (
+      infoPopUpRef.current &&
+      !infoPopUpRef.current.contains(event.target)
+    )
+      setIsPopOpen(false);
 
+    if (
+      addToListPopUpRef.current &&
+      !addToListPopUpRef.current.contains(event.target)
+    )
+      setIsAddOpen(false);
+  }
 
-  }, [infoPopUpRef, addToListPopUpRef]);
+  async function getEpisodes() {
+    const seasons = await fetchSeasons(showId);
 
-
-
-  useEffect(() => {
-    async function getEpisodes() {
-      const seasons = await fetchSeasons(showId);
-      getSavedEpisodes(showId).then((data) => {
-        if (data.length > 0) {
-          setIsWatching(true)
-          let next = data[data.length - 1]
-          let list = next.watched_episodes.split(',')
-          let last = list.lastIndexOf('true')
-          if (list.length - 1 != last) {
-            if (next.season < 10)
-              setNextEpisode(`Next: S0${next.season}-E${last + 2}`)
-            else
-              setNextEpisode(`Next: S${next.season}-E${last + 2}`)
-          }
-
-          else if (seasons.length - 1 > next.season) {
-            let nextSeason = seasons[next.season]
-            if (nextSeason.number < 10)
-              setNextEpisode(`Next: S0${nextSeason.number}-E${1}`)
-            else
-              setNextEpisode(`Next: S${nextSeason.number}-E${1}`)
-          }
-
+    getSavedEpisodes(showId).then((data) => {
+      if (data.length > 0) {
+        setIsWatching(true)
+        let next = data[data.length - 1]
+        let list = next.watched_episodes.split(',')
+        let last = list.lastIndexOf('true')
+        if (list.length - 1 !== last) {
+          if (next.season < 10)
+            setNextEpisode(`Next: S0${next.season}-E${last + 2}`)
           else
-            setNextEpisode("Completed")
-
+            setNextEpisode(`Next: S${next.season}-E${last + 2}`)
         }
-      })
 
+        else if (seasons.length > next.season) {
+          let nextSeason = seasons[next.season]
+          if (nextSeason.number < 10)
+            setNextEpisode(`Next: S0${nextSeason.number}-E${1}`)
+          else
+            setNextEpisode(`Next: S${nextSeason.number}-E${1}`)
+        }
+
+        else
+          setNextEpisode("Completed")
+
+      }
+    })
+
+  }
+
+  async function fetchData() {
+    if (isLoggedIn) {
+      await getEpisodes();
+      setIsLoading(false);
     }
+  }
 
-    getEpisodes();
+  useEffect(() => {
+    fetchData();
+  }, [isLoggedIn]);
 
-  }, [])
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [infoPopUpRef, addToListPopUpRef]);
 
   const handleInfoButton = () => {
     setIsPopOpen(true);
@@ -101,55 +109,61 @@ export default function ShowsCard({ showId, name, img, year }) {
         )}
       </label>
       <br />
-      {isWatching ?
-        (
-          <button
-            className="inline-flex border border-green-600 absoulte  items-center justify-center w-48 h-12 py-2 rounded-md"
-            onClick={() => setIsAddOpen(true)}
-          >
-            <p className="text-xl text-center text-green-600 font-montserrat">
-              {nextEpisode}
-            </p>
-          </button>
-        ) : (
-          <button
-            className="inline-flex border border-red-600 absoulte  items-center justify-center w-48 h-12 py-2 rounded-md"
-            onClick={() => setIsAddOpen(true)}
-          >
-            <p className="text-xl text-center text-red-600 font-montserrat">
-              Add to My List
-            </p>
-          </button>
-        )}
-
-      {isAddOpen && (
-        <AddToMyList
-          showId={showId}
-          popUpRef={addToListPopUpRef}
-          setIsOpen={setIsAddOpen}
-          name={name}
-        />
+      {!isLoading && (
+        <>
+          {isWatching ? (
+            <button
+              className="inline-flex border border-green-600 absoulte  items-center justify-center w-48 h-12 py-2 rounded-md"
+              onClick={() => setIsAddOpen(true)}
+            >
+              <p className="text-xl text-center text-green-600 ">{nextEpisode}</p>
+            </button>
+          ) : (
+            <button
+              className="inline-flex border border-red-600 absoulte  items-center justify-center w-48 h-12 py-2 rounded-md"
+              onClick={() => setIsAddOpen(true)}
+            >
+              <p className="text-xl text-center text-red-600">Add to My List</p>
+            </button>
+          )}
+        </>
       )}
 
-      <>
-        <button
-          className="w-9 h-8 border border-black bg-white rounded-full absolute top-0 right-0"
-          onClick={handleInfoButton}
-        >
-          <p className="text-xl text-center text-black">i</p>
-        </button>
-        {isPopOpen && (
-          <InfoPopup
-            popUpRef={infoPopUpRef}
-            setIsOpen={setIsPopOpen}
-            info={info}
-            loading={loading}
-            name={name}
-            img={img}
-            year={year}
-          />
-        )}
-      </>
+      {isLoading && <Loading />}
+
+      {isAddOpen && (
+        <>
+          {isLoggedIn ? (
+            <AddToMyList
+              showId={showId}
+              popUpRef={addToListPopUpRef}
+              setIsOpen={setIsAddOpen}
+              name={name}
+            />
+          ) : (
+            <Auth authRef={addToListPopUpRef} />
+          )}
+        </>
+      )}
+
+      <button
+        className="w-9 h-8 border border-black bg-white rounded-full absolute top-0 right-0"
+        onClick={handleInfoButton}
+      >
+        <p className="text-xl text-center text-black">i</p>
+      </button>
+
+      {isPopOpen && (
+        <InfoPopup
+          popUpRef={infoPopUpRef}
+          setIsOpen={setIsPopOpen}
+          info={info}
+          loading={isLoading}
+          name={name}
+          img={img}
+          year={year}
+        />
+      )}
 
       <br />
     </div>
