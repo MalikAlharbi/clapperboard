f3980f611e333cd2bbbcad3ee653d559fb430d30
Malik Alharbi <malik.f.alharbi@gmail.com>
2023-08-18 00:40:52 +0300

    checking if user logged in gloablly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,43 @@
 import { render } from "react-dom";
-import React from "react";
+import React, { useState, useEffect, createContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import BrowsePage from "./BrowsePage";
 import UserShowsPage from "./UserShowsPage"
+import { is_authenticated } from "./ApiRequest"
+
+
+export const AuthContext = createContext();
+
 
 export default function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function check_authentication() {
+    let response = await is_authenticated();
+    setIsLoggedIn(response);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    check_authentication();
+  }, [])
   return (
+
     <BrowserRouter>
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="browse" element={<BrowsePage />} />
-        <Route path="myshows" element={<UserShowsPage />} />
-      </Routes>
+      {!isLoading && (
+        <AuthContext.Provider value={{ isLoggedIn }}>
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="browse" element={<BrowsePage />} />
+            <Route path="myshows" element={<UserShowsPage />} />
+          </Routes>
+        </AuthContext.Provider>
+      )
+      }
+
     </BrowserRouter>
   );
 }
