72f651b3968d3a853f51f0ad3e71be50f1c10b16
MalikAlharbi <malik.f.alharbi@gmail.com>
2024-01-14 01:24:31 +0300

    activation page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { render } from "react-dom";
 import React, { useState, useEffect, createContext } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import HomePage from "./HomePage";
+import ActivationPage from "./ActivationPage";
 import BrowsePage from "./BrowsePage";
 import UserShowsPage from "./UserShowsPage";
 import ProfilePage from "./ProfilePage";
 import FriendsPage from "./FriendsPage";
-
+import NotFoundPage from "./NotFoundPage";
 import { is_authenticated, getUsername } from "./ApiRequest";
 
 export const AuthContext = createContext();
@@ -40,10 +41,14 @@ export default function App() {
           <AuthContext.Provider value={{ isLoggedIn }}>
             <Routes>
               <Route index element={<HomePage />} />
+              <Route path="activation" element={<ActivationPage />} />
               <Route path="browse" element={<BrowsePage />} />
               <Route path="myshows" element={<UserShowsPage />} />
               <Route path={`profile/:username`} element={<ProfilePage />} />
               <Route path="friends" element={<FriendsPage />} />
+              <Route path="404" element={<NotFoundPage />} />
+              {/* Redirect all unmatched routes to 404 page */}
+              <Route path="*" element={<Navigate to="/404" />} />
             </Routes>
           </AuthContext.Provider>
         </>
--- frontend/urls.py
+++ frontend/urls.py
@@ -3,6 +3,7 @@ from .views import index
 
 urlpatterns = [
     path('', index),
+    path('activation', index),
     path('browse', index),
     path('myshows', index),
     path('profile/<username>',index),
