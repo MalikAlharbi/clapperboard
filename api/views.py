65cc06e0aca13700226667023deebeb38f836a3c
Malik Alharbi <malik.f.alharbi@gmail.com>
2024-01-25 21:38:36 +0300

    password_reset backend

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 import json
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.password_validation import CommonPasswordValidator
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sessions.models import Session
@@ -45,7 +46,8 @@ from django.http import JsonResponse
 
 
 def custom_404(request, exception):
-    return redirect("http://127.0.0.1:8000/404")      
+    return redirect("http://127.0.0.1:8000/404")
+
 
 class AllUsers(generics.ListAPIView):
     permission_classes = [IsAdminUser]
@@ -63,8 +65,6 @@ class UserShows(generics.ListAPIView):
         queryset = UserShow.objects.filter(
             user=user_id).values('showId').distinct()
         return queryset
-
-
 
 
 class UserEpisodes(generics.ListAPIView):
@@ -189,18 +189,87 @@ def send_activation_email(request, user):
         current_site = get_current_site(request)
         uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
         token = default_token_generator.make_token(user)
-        activation_url = reverse('verify_email', kwargs={'uidb64': uidb64, 'token': token})
+        activation_url = reverse('verify_email', kwargs={
+                                 'uidb64': uidb64, 'token': token})
         activation_url = f'{request.scheme}://{current_site.domain}{activation_url}'
 
         email_subject = 'Activate your account!'
         email_body = f'Hi {user.username},\n\nPlease click the following link to activate your account:\n\n{activation_url}'
-        send_mail(email_subject, email_body, settings.EMAIL_HOST_USER, [user.email])
-        return JsonResponse({"success":True,'message': 'Activation email sent.'})
+        send_mail(email_subject, email_body,
+                  settings.EMAIL_HOST_USER, [user.email])
+        return JsonResponse({"success": True, 'message': 'Activation email sent.'})
     except Exception as e:
-         return JsonResponse({"success":False,'message': e})
+        return JsonResponse({"success": False, 'message': e})
 
-   
-    
+
+def send_password_email(request):
+    try:
+        data = json.loads(request.body)
+        user = User.objects.get(email=data['userEmail'])
+        current_site = get_current_site(request)
+        uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+        token = default_token_generator.make_token(user)
+        password_url = reverse('verify_password', kwargs={
+                               'uidb64': uidb64, 'token': token})
+        password_url = f'{request.scheme}://{current_site.domain}{password_url}'
+
+        email_subject = 'Password reset'
+        email_body = f'Hi {user.username},\n\nPlease click the following link to reset your password:\n\n{password_url}'
+        send_mail(email_subject, email_body,
+                  settings.EMAIL_HOST_USER, [user.email])
+        return JsonResponse({"success": True, 'message': 'Password reset email sent.'})
+    except User.DoesNotExist:
+        return JsonResponse({"success": False, 'message': "Invalid email"})
+
+
+def verify_password(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user and default_token_generator.check_token(user, token):
+        return redirect(f"http://127.0.0.1:8000/password_reset/{uidb64}/{token}", status=200)
+    return redirect("http://127.0.0.1:8000/404", status=404)
+
+
+def verify_link(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user and default_token_generator.check_token(user, token):
+        return JsonResponse({"success": True})
+
+    return JsonResponse({"success": False})
+
+
+def reset_password(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            uidb64 = data['uidb64']
+            token = data['token']
+            password = data['password']
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user and default_token_generator.check_token(user, token):
+            passwordCheck = checkPassword(password)
+            if passwordCheck is True:
+                user.password = make_password(password)
+                user.save()
+                default_token_generator.make_token(user)  # Make url invalid
+                return JsonResponse({"success": True, 'message': "Your password have been changed."})
+            else:
+                return JsonResponse({"success": False, 'error': passwordCheck})
+
+    return redirect("http://127.0.0.1:8000/404", status=404)
 
 
 def verify_email(request, uidb64, token):
@@ -213,11 +282,12 @@ def verify_email(request, uidb64, token):
     if user and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        login(request,user)
-         #redirect to activation success page
+        login(request, user)
+        # redirect to activation success page
         return redirect("http://127.0.0.1:8000/activation", status=200)
-    #redirect to 404 page
+    # redirect to 404 page
     return redirect("http://127.0.0.1:8000/404", status=404)
+
 
 def checkPassword(password):
     # Password checking
@@ -285,8 +355,6 @@ def signUp(request):
         return JsonResponse({'success': False})
 
 
-
-
 @ensure_csrf_cookie
 def is_authenticated(request):
     if (request.user.is_authenticated):
@@ -303,7 +371,6 @@ def signOut(request):
         return JsonResponse({"success": True})
 
     return JsonResponse({"success": False, "error": "error occured"})
-
 
 
 def upload_image(request):
@@ -326,9 +393,6 @@ def upload_image(request):
         return JsonResponse({"success": False, "error": "An error occurred"})
 
 
-
-
-
 def getImg(request, username):
     try:
         user = User.objects.get(username=username)
@@ -344,22 +408,24 @@ def getImg(request, username):
         return JsonResponse({"success": False, "error": "User does not exist."})
 
 
-
 @api_view(['GET'])
 def getUsername(request):
-    return JsonResponse({"success":True, "username":request.user.username})
+    return JsonResponse({"success": True, "username": request.user.username})
+
 
 @api_view(['GET'])
-def getUsernameById(request,userid):
+def getUsernameById(request, userid):
     user = User.objects.get(id=userid)
-    return JsonResponse({"success":True, "username":user.username})
+    return JsonResponse({"success": True, "username": user.username})
+
 
 @api_view(['GET'])
 def getProfileData(request, username):
     try:
         user = User.objects.get(username=username)
         date_joined = user.date_joined.strftime("%B %d, %Y")
-        total_shows = UserShow.objects.filter(user=user).values('showId').distinct().count()
+        total_shows = UserShow.objects.filter(
+            user=user).values('showId').distinct().count()
 
         # Check if the user has an active session
         online = False
@@ -370,7 +436,8 @@ def getProfileData(request, username):
                 online = True
                 break
 
-        current_status = 'Online' if online else user.last_login.strftime("%T - %B %d, %Y") if (user.last_login and online is False) else ''
+        current_status = 'Online' if online else user.last_login.strftime(
+            "%T - %B %d, %Y") if (user.last_login and online is False) else ''
 
         return JsonResponse({
             "success": True,
@@ -383,23 +450,24 @@ def getProfileData(request, username):
     except Exception as e:
         return JsonResponse({"success": False, "error": str(e)})
 
-    
+
 @api_view(['GET'])
 def getProfileShows(request, username):
     user = User.objects.get(username=username)
-    showsJson = list(UserShow.objects.filter(user=user).values('showId').distinct())
-    return JsonResponse({"success":True,"showsJson":showsJson})
-
+    showsJson = list(UserShow.objects.filter(
+        user=user).values('showId').distinct())
+    return JsonResponse({"success": True, "showsJson": showsJson})
 
 
 @api_view(['GET'])
-def getProfileEpisodes(request,show_id,username):
+def getProfileEpisodes(request, show_id, username):
     user = User.objects.get(username=username)
-    queryset = list(UserShow.objects.filter(user=user, showId = show_id).values().order_by('season'))
-    return JsonResponse({"success":True,"showsJson":queryset})
+    queryset = list(UserShow.objects.filter(
+        user=user, showId=show_id).values().order_by('season'))
+    return JsonResponse({"success": True, "showsJson": queryset})
 
 
-#Friends
+# Friends
 
 @api_view(['GET'])
 def showFriends(request):
@@ -407,20 +475,23 @@ def showFriends(request):
     serializer = FriendshipSerializer(friends, many=True)
     return JsonResponse({"success": True, "friends": serializer.data})
 
+
 def friendshipStatus(request, user):
-    isSenderPending = FriendRequest.objects.filter(from_user=request.user, to_user=user).exists()
-    isReceiverPending = FriendRequest.objects.filter(from_user=user, to_user=request.user).exists()
-    isUserAfriend = Friendship.objects.filter(user_1=request.user, user_2=user).exists()
+    isSenderPending = FriendRequest.objects.filter(
+        from_user=request.user, to_user=user).exists()
+    isReceiverPending = FriendRequest.objects.filter(
+        from_user=user, to_user=request.user).exists()
+    isUserAfriend = Friendship.objects.filter(
+        user_1=request.user, user_2=user).exists()
 
     if isSenderPending:
-     return JsonResponse({"success": True, "friendshipStatus": 'Pending_sender'})
+        return JsonResponse({"success": True, "friendshipStatus": 'Pending_sender'})
 
     if isReceiverPending:
-     return JsonResponse({"success": True, "friendshipStatus": 'Pending_receiver'})
+        return JsonResponse({"success": True, "friendshipStatus": 'Pending_receiver'})
 
     if isUserAfriend:
         return JsonResponse({"success": True, "friendshipStatus": 'Friend'})
-     
 
     return JsonResponse({"success": True, "friendshipStatus": 'Not a friend'})
 
@@ -431,16 +502,18 @@ def sendFriendReq(request):
     to_user = User.objects.get(username=username)
     if Friendship.objects.filter(user_1=request.user, user_2=to_user).exists():
         return JsonResponse({"success": False, "message": "Already friends"})
-    if FriendRequest.objects.filter(from_user = to_user, to_user = request.user):
+    if FriendRequest.objects.filter(from_user=to_user, to_user=request.user):
         return JsonResponse({"success": False, "message": "Friend request already sent from the other user"})
-    if(request.user.username == username):
+    if (request.user.username == username):
         return JsonResponse({"success": False, "message": "Error"})
 
-    friendReq, created = FriendRequest.objects.get_or_create(from_user=request.user, to_user=to_user)
+    friendReq, created = FriendRequest.objects.get_or_create(
+        from_user=request.user, to_user=to_user)
     if not created:
         return JsonResponse({"success": False, "message": "Friend Request already sent"})
 
     return JsonResponse({"success": True, "message": "Friend request sent"})
+
 
 @api_view(['GET'])
 def showFriendReq(request):
@@ -448,40 +521,43 @@ def showFriendReq(request):
     serializer = FriendRequestSerializer(friendReq, many=True)
     return JsonResponse({"success": True, "friendReq": serializer.data})
 
+
 def friendReqDecision(request):
     data = json.loads(request.body)
     username = data["username"]
     decision = data["decision"]
     from_user = User.objects.get(username=username)
-    friendReq = FriendRequest.objects.get(from_user=from_user, to_user=request.user)
+    friendReq = FriendRequest.objects.get(
+        from_user=from_user, to_user=request.user)
     if not decision:
         friendReq.delete()
-        return JsonResponse({"success": True, "decision":"decline"})
-    
-    obj1, created1 = Friendship.objects.get_or_create(user_1=request.user, user_2= from_user)
-    obj2, created2 = Friendship.objects.get_or_create(user_1=from_user, user_2= request.user)
+        return JsonResponse({"success": True, "decision": "decline"})
+
+    obj1, created1 = Friendship.objects.get_or_create(
+        user_1=request.user, user_2=from_user)
+    obj2, created2 = Friendship.objects.get_or_create(
+        user_1=from_user, user_2=request.user)
     friendReq.delete()
     return JsonResponse({"success": True, "decision": "accepet"})
 
+
 def deleteFriend(request):
-#    Friendship.objects.get(user=request.user, friend= username).delete()
+    #    Friendship.objects.get(user=request.user, friend= username).delete()
     data = json.loads(request.body)
     username = data["username"]
     user_id = User.objects.get(username=username)
-    get_object_or_404(Friendship, user_1=request.user, user_2= user_id).delete()
-    get_object_or_404(Friendship, user_1=user_id, user_2= request.user).delete()
+    get_object_or_404(Friendship, user_1=request.user, user_2=user_id).delete()
+    get_object_or_404(Friendship, user_1=user_id, user_2=request.user).delete()
     return JsonResponse({"success": True, "message": "Friend Deleted"})
-        
-
 
 
 def searchForUser(request, username):
     if not username:
         return JsonResponse({"success": False, "message": "No username provided"})
-    searchResult = User.objects.filter(username__icontains=username).exclude(username=request.user).order_by(Length('username').asc())
+    searchResult = User.objects.filter(username__icontains=username).exclude(
+        username=request.user).order_by(Length('username').asc())
     if not searchResult:
         return JsonResponse({"success": False, "message": "No matching users found"})
 
     searchList = list(searchResult.values_list('id', flat=True))
-    return  JsonResponse({"success":True,'user_ids': searchList})
-
+    return JsonResponse({"success": True, 'user_ids': searchList})
