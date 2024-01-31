fdb20260a4e1763db61c1fd5a69a031cf72b6507
Malik Alharbi <malik.f.alharbi@gmail.com>
2024-01-31 21:03:30 +0300

    DEBUG = False

--- clapperboard/settings.py
+++ clapperboard/settings.py
@@ -30,7 +30,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
